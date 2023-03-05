4a234588548069ccacc3995bcb8b83f1ebf727d2
Steph <stephane@molano.fr>
2023-03-05 08:40:42 +0100

    Unit test & composer fix

--- tests/Unit/Services/Spotify/SpotifyApiSyncTest.php
+++ tests/Unit/Services/Spotify/SpotifyApiSyncTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Services\Spotify;
 
+use App\Exceptions\Services\Spotify\SpotifyAuthException;
 use App\Models\Playlist;
 use App\Models\Song;
 use App\Models\SpotifyPlaylist;
@@ -31,52 +32,99 @@ class SpotifyApiSyncTest extends TestCase
         $this->expectException(\Exception::class);
         app(SpotifyApiSync::class)->sync();
 
-        $this->expectException(\SpotifyAuthException::class);
+        $this->expectException(SpotifyAuthException::class);
         app(SpotifyApiSync::class)
             ->setPlaylist(Playlist::factory()->make())
             ->sync();
     }
 
-     public function test_sync_spotify_playlist_created_if_doesnt_exist()
-     {
-         SpotifyApiClientMock::make()
-             ->makeRequestAccessTokenSessionMock()
-             ->makeSpotifyWebApiMock(function ($mock) {
-                 $mock
-                     ->shouldReceive('me')
-                     ->andReturn(SpotifyUserFixtures::getMe())
-                     ->shouldReceive('createPlaylist')
-                     ->andReturn(SpotifyPlaylistFixtures::getCreatedPlaylist())
-                     ->shouldReceive('getPlaylist')
-                     ->andReturn(SpotifyPlaylistFixtures::getBasicPlaylist())
-                     ->shouldReceive('getPlaylistTracks')
-                     ->times(2)
-                     ->andReturn(SpotifyPlaylistFixtures::getPlaylistTracksIds())
-                     ->shouldReceive('addPlaylistTracks')
-                     ->with('123456', ['280OLJ1XsWyuEFTiiDg54q']);
-             })
-             ->bind();
+    public function test_sync_spotify_playlist_created_if_doesnt_exist()
+    {
+        SpotifyApiClientMock::make()
+            ->makeRequestAccessTokenSessionMock()
+            ->makeSpotifyWebApiMock(function ($mock) {
+                $mock
+                    ->shouldReceive('me')
+                    ->andReturn(SpotifyUserFixtures::getMe())
+                    ->shouldReceive('createPlaylist')
+                    ->andReturn(SpotifyPlaylistFixtures::getCreatedPlaylist())
+                    ->shouldReceive('getPlaylist')
+                    ->andReturn(SpotifyPlaylistFixtures::getBasicPlaylist())
+                    ->shouldReceive('getPlaylistTracks')
+                    ->times(2)
+                    ->andReturn(SpotifyPlaylistFixtures::getPlaylistTracksIds())
+                    ->shouldReceive('addPlaylistTracks')
+                    ->once()
+                    ->with('123456', ['AAA']);
+            })
+            ->bind();
 
-         $songToSync = Song::factory([
-             'spotify_id' => '280OLJ1XsWyuEFTiiDg54q',
-         ])->create();
+        $songToSync = Song::factory([
+            'spotify_id' => 'AAA',
+        ])->create();
 
-         $playlist = Playlist::factory()
-             ->hasAttached($songToSync)
-             ->create();
+        $playlist = Playlist::factory()
+            ->hasAttached($songToSync)
+            ->create();
 
-         $response = app(SpotifyApiSync::class)
-             ->setPlaylist($playlist)
-             ->sync();
+        $response = app(SpotifyApiSync::class)
+            ->setPlaylist($playlist)
+            ->sync();
 
-         $spotifyPlaylist = SpotifyPlaylist::where('playlist_id', $playlist->id)->first();
+        $spotifyPlaylist = SpotifyPlaylist::where('playlist_id', $playlist->id)->first();
 
-         $this->assertDatabaseHas(SpotifyPlaylist::class, [
-             'playlist_id' => $playlist->id,
-         ]);
+        $this->assertDatabaseHas(SpotifyPlaylist::class, [
+            'playlist_id' => $playlist->id,
+        ]);
 
-         $this->assertEquals($playlist->id, data_get($response, 'spotifyPlaylist.playlist_id'));
-         $this->assertEquals($spotifyPlaylist->snapshot_id, '345');
-         $this->assertEquals($spotifyPlaylist->songs->first()->spotify_id, '280OLJ1XsWyuEFTiiDg54q');
-     }
+        $this->assertEquals($playlist->id, data_get($response, 'spotifyPlaylist.playlist_id'));
+        $this->assertEquals($spotifyPlaylist->snapshot_id, '345');
+        $this->assertEquals($spotifyPlaylist->songs->first()->spotify_id, 'AAA');
+    }
+
+    public function test_playlist_creation_issue_throws_exception()
+    {
+        SpotifyApiClientMock::make()
+            ->makeRequestAccessTokenSessionMock()
+            ->makeSpotifyWebApiMock(function ($mock) {
+                $mock
+                    ->shouldReceive('me')
+                    ->andReturn(SpotifyUserFixtures::getMe())
+                    ->shouldReceive('createPlaylist')
+                    ->andReturn(null);
+            })
+            ->bind();
+
+        $songToSync = Song::factory([
+            'spotify_id' => '280OLJ1XsWyuEFTiiDg54q',
+        ])->create();
+
+        $playlist = Playlist::factory()
+            ->hasAttached($songToSync)
+            ->create();
+
+        $this->expectException(\Exception::class);
+
+        app(SpotifyApiSync::class)
+            ->setPlaylist($playlist)
+            ->sync();
+    }
+
+    public function test_requisite_fails_if_cant_retrieve_user()
+    {
+        SpotifyApiClientMock::make()
+            ->makeRequestAccessTokenSessionMock()
+            ->makeSpotifyWebApiMock(function ($mock) {
+                $mock
+                    ->shouldReceive('me')
+                    ->andReturn(null);
+            })
+            ->bind();
+
+        $this->expectException(SpotifyAuthException::class);
+
+        app(SpotifyApiSync::class)
+            ->setPlaylist(Playlist::factory()->make())
+            ->sync();
+    }
 }
