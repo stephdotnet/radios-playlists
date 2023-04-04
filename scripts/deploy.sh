19e1c53f7a6cf9d1328351379ca01879a111bf1e
Steph <7047375+stephdotnet@users.noreply.github.com>
2023-04-04 07:50:05 +0200

    fix: composer install --no-dev

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@
 git fetch && git pull
 
 # Run composer install in the webserver container of the docker-compose file
-docker-compose run --rm webserver composer install
+docker-compose run --rm webserver composer install --no-dev
 
 # Run migrations in the webserver container of the docker-compose file
 docker-compose run --rm webserver php artisan migrate --force
