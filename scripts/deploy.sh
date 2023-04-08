55ee397756209228d5ee46cdd405afd58fa430c7
Stephdotnet <stephane@molano.fr>
2023-04-08 09:11:45 +0200

    chore: Update npm dependencies (#13)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Pull latest changes from the git repository
-git fetch && git pull
+cd /home/lamp/www/radios && git fetch && git pull
 
 # Run composer install in the webserver container of the docker-compose file
 docker-compose -f /home/lamp/docker-compose.yml run --rm -w /var/www/html/radios webserver composer install
