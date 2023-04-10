2b88be581c282be02e09e3a2c8e5d256e1a23fa0
stephdotnet <stephane@molano.fr>
2023-04-10 16:23:43 +0200

    chore: added social branding

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,6 +1,33 @@
 <html>
     <head>
+        @php
+            $title = "Radios Playlists - Creating playlists from your radios while you sleep";
+            $description= "This pre-alpha app's goal is to listen your favorite radios for you, and to make playlists that you can listen later.";    
+        @endphp
+        <!-- Primary Meta Tags -->
+        <meta name="title" content="{{ $title }}">
+        <meta name="description" content="This pre-alpha app's goal is to listen your favorite radios for you, and to make playlists that you can listen later.">
+
         <title>{{ $title }}</title>
+
+        <!-- Favicon -->  
+        <link rel="icon" type="image/svg+xml" href="{{ asset('favicon/favicon.svg') }}">
+        <link rel="icon" type="image/png" href="{{ asset('favicon/favicon.png') }}">              
+        
+        <!-- Open Graph / Facebook -->
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ route('index') }}">
+        <meta property="og:title" content="{{ $title }}">
+        <meta property="og:description" content="{{ $description }}">
+        <meta property="og:image" content="{{ asset('images/social.jpg') }}">
+
+        <!-- Twitter -->
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="{{ route('index') }}">
+        <meta property="twitter:title" content="{{ $title }}">
+        <meta property="twitter:description" content="{{ $description }}">
+        <meta property="twitter:image" content="{{ asset('images/social.jpg') }}">        
+
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link
             rel="stylesheet"
