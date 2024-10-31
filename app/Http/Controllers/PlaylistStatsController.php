043a4947006d0251e2ae6b6b71d2d279cf5868d1
Stephdotnet <stephane@molano.fr>
2024-10-31 09:49:24 +0100

    fix: added stats cache

--- app/Http/Controllers/PlaylistStatsController.php
+++ app/Http/Controllers/PlaylistStatsController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Playlist;
 use App\Services\Playlist\Stats;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class PlaylistStatsController extends Controller
 {
     public function show(Playlist $playlist, Stats $stats)
     {
-        return $stats->setPlaylist($playlist)->make()->json();
+        return Cache::remember(
+            'playlist.stats.id:' . $playlist->id,
+            Carbon::now()->addHours(6),
+            function () use ($playlist, $stats) {
+                return $stats->setPlaylist($playlist)->make()->json();
+            },
+        );
     }
 }
